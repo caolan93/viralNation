190fb5897b65e159455822d56452f3b1f8f70a8c
caolan <caolan@digigrow.ie>
2023-05-11 18:14:35 -0400

    Grid card component and custom style sheet

---
+++ src/components/grid/Card.tsx
@@ -0,0 +1,47 @@
+import { Card, Typography, CardContent, Avatar } from "@mui/material";
+import { MoreVert } from "@mui/icons-material";
+
+// Styling
+import "../../styles/grid/gridCard.scss";
+
+const GridCard = ({ content }: { content: DataType }) => {
+  console.log("CONTENT", content);
+  return (
+    <Card
+      className="grid-card-container"
+      sx={{
+        backgroundColor: "#EEEEEE",
+        borderRadius: "8px",
+        padding: "0",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <CardContent sx={{ padding: "24px" }}>
+        <div className="grid-card-header">
+          <Avatar
+            sx={{ height: "64px", width: "64px" }}
+            alt={`Image of ${content?.name}`}
+            src={content?.image}
+          />
+          <div className="grid-card-text-info">
+            <Typography variant="body1" fontWeight={500} color="#212121">
+              {content?.name}
+            </Typography>
+            <Typography variant="body2" fontWeight={400} color="#2B2B2B">
+              {content?.email}
+            </Typography>
+          </div>
+          <MoreVert />
+        </div>
+        <div>
+          <Typography variant="caption" fontWeight={400} color="#616161">
+            {content?.description}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default GridCard;
