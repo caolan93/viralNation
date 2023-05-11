86ee55e2fa958d5bf711eedc312f291f14459edf
caolan <caolan@digigrow.ie>
2023-05-11 13:18:36 -0400

    adding V logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <Navbar />
     </>
   );
 }
