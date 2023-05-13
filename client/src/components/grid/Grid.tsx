6b0e8552f51f73fa3fd3e2b3aa0361a2244f20f0
caolan <caolan@digigrow.ie>
2023-05-13 16:11:58 -0400

    adding in loading file when fetching data

--- client/src/components/grid/Grid.tsx
+++ client/src/components/grid/Grid.tsx
@@ -19,11 +19,6 @@ import "../../styles/components/grid/grid.scss";
 const GridPage = () => {
   const { loading, error, data } = useQuery(GET_USERS);
 
-  if (loading) {
-    return console.log("loading still");
-  } else if (error) {
-    return console.log("error still");
-  }
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClose = () => {
@@ -33,65 +28,87 @@ const GridPage = () => {
     setIsOpen(true);
   };
 
+  if (loading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <p>Something Went Wrong</p>
+      </>
+    );
+  }
+
   return (
-    <div className="grid-container">
-      <Grid justifyContent="center" container>
-        <Grid lg={9} xs={12} item>
-          <Grid className="input-container" item xs={12}>
-            <Grid item xs={12}>
-              <TextField fullWidth />
-            </Grid>
-            <Grid className="buttons-container" item xs={12} lg={3}>
-              <Button
-                onClick={handleOpen}
-                color="primary"
-                variant="outlined"
-                startIcon={<PersonAdd sx={{ height: "16px", width: "16px" }} />}
-              >
-                <Typography
-                  sx={{ textTransform: "none", whiteSpace: "pre" }}
-                  variant="body2"
-                >
-                  Create Profile
-                </Typography>
-              </Button>
-              <Hidden smDown={true}>
-                <Button
-                  className="layout-switch-btn"
-                  variant="outlined"
-                  startIcon={
-                    <ViewWeek sx={{ height: "20px", width: "20px" }} />
-                  }
-                  endIcon={<List sx={{ height: "25px", width: "25px" }} />}
-                />
-              </Hidden>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid justifyContent="center" container>
-        <Grid lg={9} xs={12} item>
-          <Grid spacing={{ xs: 0, sm: 3 }} rowSpacing={{ xs: 3 }} container>
-            {data?.users?.map((value: DataType, index: number) => (
-              <Grid
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                key={index + 1}
-                xs={12}
-                md={6}
-                lg={3}
-                item
-              >
-                <GridCard content={value} isFormOpen={handleOpen} />
+    <>
+      {!loading && !error && (
+        <div className="grid-container">
+          <Grid justifyContent="center" container>
+            <Grid lg={9} xs={12} item>
+              <Grid className="input-container" item xs={12}>
+                <Grid item xs={12}>
+                  <TextField fullWidth />
+                </Grid>
+                <Grid className="buttons-container" item xs={12} lg={3}>
+                  <Button
+                    onClick={handleOpen}
+                    color="primary"
+                    variant="outlined"
+                    startIcon={
+                      <PersonAdd sx={{ height: "16px", width: "16px" }} />
+                    }
+                  >
+                    <Typography
+                      sx={{ textTransform: "none", whiteSpace: "pre" }}
+                      variant="body2"
+                    >
+                      Create Profile
+                    </Typography>
+                  </Button>
+                  <Hidden smDown={true}>
+                    <Button
+                      className="layout-switch-btn"
+                      variant="outlined"
+                      startIcon={
+                        <ViewWeek sx={{ height: "20px", width: "20px" }} />
+                      }
+                      endIcon={<List sx={{ height: "25px", width: "25px" }} />}
+                    />
+                  </Hidden>
+                </Grid>
               </Grid>
-            ))}
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
-      <Form isOpen={isOpen} handleClose={handleClose} />
-    </div>
+          <Grid justifyContent="center" container>
+            <Grid lg={9} xs={12} item>
+              <Grid spacing={{ xs: 0, sm: 3 }} rowSpacing={{ xs: 3 }} container>
+                {data?.users?.map((value: DataType, index: number) => (
+                  <Grid
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                    key={index + 1}
+                    xs={12}
+                    md={6}
+                    lg={3}
+                    item
+                  >
+                    <GridCard content={value} isFormOpen={handleOpen} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Form isOpen={isOpen} handleClose={handleClose} />
+        </div>
+      )}
+    </>
   );
 };
 
