0a8d17be441dc01efe841fd36bc11a6e8aeafc0d
caolan <caolan@digigrow.ie>
2023-05-12 21:28:58 -0400

    creating dummy data

---
+++ src/assets/dataArr.ts
@@ -0,0 +1,88 @@
+export const data = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    description: "Software engineer with a passion for music and photography.",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    is_verified: false,
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+    description: "Freelance writer and editor with a love for coffee and cats.",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    email: "bobjohnson@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/men/3.jpg",
+    description:
+      "Marketing professional with a passion for travel and adventure.",
+  },
+  {
+    id: 4,
+    name: "Emily Rodriguez",
+    email: "emilyrodriguez@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+    description:
+      "Graphic designer with a love for all things vintage and retro.",
+  },
+  {
+    id: 5,
+    name: "David Lee",
+    email: "davidlee@example.com",
+    is_verified: false,
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    description:
+      "Aspiring chef with a passion for fusion cuisine and molecular gastronomy.",
+  },
+  {
+    id: 6,
+    name: "Sarah Kim",
+    email: "sarahkim@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    description:
+      "Fashion blogger with a love for vintage clothing and streetwear.",
+  },
+  {
+    id: 7,
+    name: "Alex Wong",
+    email: "alexwong@example.com",
+    is_verified: false,
+    image: "https://randomuser.me/api/portraits/men/7.jpg",
+    description:
+      "Fitness enthusiast with a passion for strength training and bodybuilding.",
+  },
+  {
+    id: 8,
+    name: "Karen Chen",
+    email: "karenchen@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/women/8.jpg",
+    description: "Yoga instructor with a love for mindfulness and meditation.",
+  },
+  {
+    id: 9,
+    name: "Michael Kim",
+    email: "michaelkim@example.com",
+    is_verified: true,
+    image: "https://randomuser.me/api/portraits/men/9.jpg",
+    description:
+      "Artist and illustrator with a passion for comics and graphic novels.",
+  },
+  {
+    id: 10,
+    name: "Jennifer Lee",
+    email: "jenniferlee@example.com",
+    is_verified: false,
+    image: "https://randomuser.me/api/portraits/women/10.jpg",
+    description: "Software engineer with a passion for music and photography.",
+  },
+];
