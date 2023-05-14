7b7d2e1dbd78f692957312c60017d85338bbda17
caolan <caolan@digigrow.ie>
2023-05-14 17:07:07 -0400

    Adding a footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,57 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Switch,
+  BottomNavigation,
+} from "@mui/material";
+import { DarkMode } from "@mui/icons-material";
+import { LightMode } from "@mui/icons-material";
+
+const Footer = () => {
+  return (
+    <BottomNavigation
+      sx={{
+        position: "fixed",
+        width: "100%",
+        bottom: 0,
+        background: "#FFF",
+        p: 1,
+        display: "flex",
+        gap: 4,
+        boxShadow: "0px 1px 12px 0px #000000",
+      }}
+    >
+      <Typography
+        color="#121212"
+        sx={{ display: "flex", alignItems: "center", fontWeight: "400" }}
+        variant="body2"
+      >
+        <svg
+          width="33"
+          height="32"
+          viewBox="0 0 33 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14.5625 0V0.605966H10.3731L19.4005 24.0676L28.6008 0.605966H24.6723V0H33V0.605966H29.2536L16.9639 32H15.6992L3.66277 0.605969H0V0H14.5625Z"
+            fill="#121212"
+          />
+        </svg>
+        iral Nation
+      </Typography>
+      <Typography
+        color="#121212"
+        sx={{ display: "flex", alignItems: "center", fontWeight: "400" }}
+        variant="body2"
+      >
+        All Rights Reserved 2023.{" "}
+        <span style={{ fontWeight: "bolder" }}>&copy;</span>
+      </Typography>
+    </BottomNavigation>
+  );
+};
+
+export default Footer;
