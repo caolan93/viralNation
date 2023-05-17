a6f02e6780768542a98a92cc3f2cc5478ae50cab
caolan <caolan@digigrow.ie>
2023-05-16 20:26:19 -0400

    clean up schema

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -30,12 +30,6 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    // users: {
-    //   type: new GraphQLList(UserType),
-    //   resolve(parent, args) {
-    //     return User.find({});
-    //   },
-    // },
     users: {
       type: new GraphQLList(UserType),
       args: {
